c6da48a42c981fbebc272b50a023a04f224df256
yomuki <yomuki@qq.com>
2023-03-28 15:20:32 +0800

    笔记

--- utils/gather/index.d.ts
+++ utils/gather/index.d.ts
@@ -5,11 +5,13 @@ declare type Concurrence<A, B> = A | B
 
 /**
  * 交集
+ * 分布式条件类型
  */
 declare type Intersection<A, B> = A extends B ? A : never
 
 /**
  * 差集
+ * 返回的为A的子集
  */
 declare type Difference<A, B> = A extends B ? never : A
 
@@ -42,4 +44,39 @@ declare type ObjectKeyDifference<T extends PlainObject, U extends PlainObject> =
 /**
  * 属性名补集
  */
-declare type ObjectKeyComplement<T extends PlainObject, U extends PlainObject> = Complement<keyof T, keyof U>
+declare type ObjectKeyComplement<T extends U, U extends PlainObject> = Complement<keyof T, keyof U>
+
+/**
+ * 对象交集
+ */
+declare type ObjectIntersection<T extends PlainObject, U extends PlainObject> = Pick<T, ObjectKeyIntersection<T, U>>
+
+/**
+ * 对象差集
+ * 返回T比U多的部分
+ */
+declare type ObjectDifference<T extends PlainObject, U extends PlainObject> = Pick<T, ObjectKeyDifference<T, U>>
+
+/**
+ * 对象补集
+ */
+declare type ObjectComplement<T extends U, U extends PlainObject> = Pick<T, ObjectKeyComplement<T, U>>
+
+/**
+ * 对象并集
+ * T比U多的部分 +  T与U的交集 + U比T多的部分
+ * U的优先级比T高
+ */
+declare type ObjectMerge<T extends PlainObject, U extends PlainObject> = ObjectDifference<T, U> & ObjectIntersection<U, T> & ObjectDifference<U, T>
+
+/**
+ * 对象合并
+ * T的优先级比U高
+ */
+declare type ObjectAssign<T extends PlainObject, U extends PlainObject> = ObjectDifference<T, U> & ObjectIntersection<T, U> & ObjectDifference<U, T>
+
+/**
+ * 对象重写
+ * 将U的属性覆盖T的属性,但U中独有的属性不会被添加
+ */
+declare type ObjectOverride<T extends PlainObject, U extends PlainObject> = ObjectDifference<T, U> & ObjectIntersection<U, T>
--- utils/gather/test/index.test.ts
+++ utils/gather/test/index.test.ts
@@ -1,17 +1,38 @@
 import { expectType } from 'tsd'
-
+/**
+ * 并集
+ */
 type ConcurrenceTest = Concurrence<1 | 2 | 3, 1 | 2> // 123
-
-type IntersectionTest = Intersection<1 | 2 | 3, 1 | 2> // 12
-
-type DifferenceTest = Difference<1 | 2 | 3, 1 | 2> // 3
-
-type ComplementTest = Complement<1 | 2 | 3, 1 | 2> // 3
-
 type ObjectKeyConcurrenceTest = ObjectKeyConcurrence<{ a: 1 }, { b: 2 }> // 'a' | 'b'
+/**
+ * 交集
+ */
+type IntersectionTest1 = Intersection<'a' | 'b', 'a'> // a
+type IntersectionTest2 = Intersection<'a', 'a' | 'b'> // a
+type ObjectKeyIntersectionTest = ObjectKeyIntersection<{ a: string ;b: number}, { a: string }> // a
+type ObjectIntersectionTest = ObjectIntersection<{ a: string ;b: number}, { b: number }> // {b:number}
+type ObjectIntersectionTest2 = ObjectIntersection<{ a: string ;b: number}, { b: string }>// {b:number} 如果属性名相同，类型不同，取前者类型
 
-type ObjectKeyIntersectionTest = ObjectKeyIntersection<{ a: 1 }, { b: 2 }> // never
-
+/**
+ * 差集
+ */
+type DifferenceTest = Difference<'a' | 'b', 'a'> // b
+type DifferenceTest2 = Difference<'a', 'a' | 'b'> // never
+type DifferenceTest3 = Difference<'a', 'b'> // a
 type ObjectKeyDifferenceTest = ObjectKeyDifference<{ a: 1 }, { b: 2 }> // 'a'
 
-type ObjectKeyComplementTest = ObjectKeyComplement<{ a: 1 }, { b: 2 }> // 'a'
+/**
+ * 补集
+ */
+type ComplementTest = Complement<'a' | 'b', 'a'> // b
+type ObjectKeyComplementTest = ObjectKeyComplement<{ a: string; b: number }, { a: string }> // 'b'
+
+/**
+ * 对象合并
+ */
+type ObjectMergeTest = ObjectMerge<{ a: string ;b: number}, { b: string; c: boolean }>
+expectType<ObjectMergeTest>({ a: 'a', b: 'b', c: true })
+type ObjectAssignTest = ObjectAssign<{ a: string ;b: number}, { b: string; c: boolean }>
+expectType<ObjectAssignTest>({ a: 'a', b: 1, c: true })
+type ObjectOverrideTest = ObjectOverride<{ a: string ;b: number}, { b: string; c: boolean }>
+expectType<ObjectOverrideTest>({ a: 'a', b: 'b' })
